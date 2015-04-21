65983b802efe4ae8c0e7012bd6e0f872a4630f7b
Anas Hammani <ahammani@excilys.com>
2015-04-21 15:19:34 +0200

    Add a better properties file

--- computer-database/src/main/java/com/excilys/cdb/dao/FactoryConnection.java
+++ computer-database/src/main/java/com/excilys/cdb/dao/FactoryConnection.java
@@ -1,29 +1,50 @@
 package com.excilys.cdb.dao;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.excilys.cdb.exception.DAOException;
-
-import static com.excilys.cdb.database.DatabaseNaming.*;
 
 public enum FactoryConnection {
 	INSTANCE;
 
-	static {
+	private InputStream input;
+	private Properties prop = new Properties();
+
+	FactoryConnection() {
 		try {
-			Class.forName(DRIVER);
+			input = this.getClass().getClassLoader()
+					.getResourceAsStream("config.properties");
+			prop.load(input);
+			Class.forName(prop.getProperty("driver"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public String getProp(String s) {
+		return FactoryConnection.INSTANCE.prop.getProperty(s);
+	}
+
 	public Connection openConnection() {
 		try {
-			return DriverManager.getConnection(URL, USER, PWD);
+			return DriverManager.getConnection(
+					FactoryConnection.INSTANCE.getProp("url"),
+					FactoryConnection.INSTANCE.getProp("user"),
+					FactoryConnection.INSTANCE.getProp("pwd"));
 		} catch (SQLException e) {
 			throw new DAOException();
 		}
