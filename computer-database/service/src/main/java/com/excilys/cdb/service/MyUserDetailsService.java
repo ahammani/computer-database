4822a5cdb63a615a41a9ce510d13d5aedfdfa1cc
Anas Hammani <ahammani@excilys.com>
2015-05-21 13:37:20 +0200

    Add Digest HTTP Auth

--- computer-database/service/src/main/java/com/excilys/cdb/service/MyUserDetailsService.java
+++ computer-database/service/src/main/java/com/excilys/cdb/service/MyUserDetailsService.java
@@ -2,8 +2,6 @@ package com.excilys.cdb.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.excilys.cdb.dao.UserDAO;
 import com.excilys.cdb.model.UserRole;
@@ -21,6 +20,7 @@ public class MyUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UserDAO userDAO;
 
+	@Transactional(readOnly = true)
 	@Override
 	public UserDetails loadUserByUsername(String username)
 			throws UsernameNotFoundException {
